b31303064f7b53ac2aa959d8e3c5e519cd1b2c88
Newson Rai <firstnepseking@gmail.com>
2025-05-01 14:30:00 +0545

    add gets book api

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express') //express require gareko 
+const { books } = require('./database/connection')
+
+
 const app = express()//express trigger gareko
 
 //import gareko databaseko file
 require("./database/connection")
 
+app.use(express.json())
+
 // Alternative Method
 // const app = require('express')()
 
-app.get("/books",(req,res)=>{
+app.get("/books",async(req,res)=>{
+// logic to fetch books from database
+const datas = await books.findAll() //select * from books, always returns arrays
+
     res.json({
-        message : "Books fetched successfully."
+        message : "Books fetched successfully.",
+        datas
     })
 })
 app.post("/books",(req,res)=>{
+    console.log(req.body)
     res.json({
         message : "Book added successfully."
     })
@@ -34,6 +44,6 @@ app.patch("/books/:id",(req,res)=>{
 
 postgresql://postgres.umzlbthbzliwutpabqfc:Newson@01@aws-0-ap-south-1.pooler.supabase.com:6543/postgres
 
-app.listen(3000,function(){
+app.listen(4000,function(){
     console.log('server/backend/node project has started at port 3000')
 })
